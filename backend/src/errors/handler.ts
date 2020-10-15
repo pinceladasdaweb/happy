bd2f5e502ef521051e0807c2cffb497c115237fc
Pedro Rogério <pinceladasdaweb@hotmail.com>
2020-10-15 17:28:41 -0300

    Fix indentation

--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -2,19 +2,19 @@ import { ValidationError } from 'yup'
 import { ErrorRequestHandler } from 'express'
 
 interface ValidationErrors {
-	[key: string]: string[]
+  [key: string]: string[]
 }
 
 const errorHanlder: ErrorRequestHandler = (error, request, respoonse, next) => {
-	if (error instanceof ValidationError) {
-		let errors: ValidationErrors = {}
+  if (error instanceof ValidationError) {
+    let errors: ValidationErrors = {}
 
-		error.inner.forEach(err => {
-			errors[err.path] = err.errors
-		})
+    error.inner.forEach(err => {
+      errors[err.path] = err.errors
+    })
 
-		return respoonse.status(400).json({ message: 'Validation fails', errors })
-	}
+    return respoonse.status(400).json({ message: 'Validation fails', errors })
+  }
   console.error(error)
 
   return respoonse.status(500).json({ message: 'Internal server error' })
