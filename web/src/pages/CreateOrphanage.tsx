485907504d4b92909a9d453257e75492944d8ff7
Pedro Rogério <pinceladasdaweb@hotmail.com>
2020-10-17 16:37:48 -0300

    Import Sidebar component

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -1,33 +1,14 @@
-import L from 'leaflet'
 import React from 'react'
-import { useHistory } from 'react-router-dom'
+import mapIcon from '../utils/mapIcon'
+import { FiPlus } from 'react-icons/fi'
+import Sidebar from '../components/Sidebar'
 import '../styles/pages/create-orphanage.css'
-import mapMarkerImg from '../images/map-marker.svg'
-import { FiArrowLeft, FiPlus } from 'react-icons/fi'
 import { Map, Marker, TileLayer } from 'react-leaflet'
 
-const happyMapIcon = L.icon({
-  iconUrl: mapMarkerImg,
-
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [0, -60]
-})
-
 export default function CreateOrphanage() {
-  const { goBack } = useHistory()
-
   return (
     <div id="page-create-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
-
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
+      <Sidebar />
 
       <main>
         <form className="create-orphanage-form">
@@ -43,7 +24,7 @@ export default function CreateOrphanage() {
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
 
-              <Marker interactive={false} icon={happyMapIcon} position={[-27.2092052,-49.6401092]} />
+              <Marker interactive={false} icon={mapIcon} position={[-27.2092052,-49.6401092]} />
             </Map>
 
             <div className="input-block">
