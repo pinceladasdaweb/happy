a41fe2039a34e66ff56c1f8c7a08839c356e87b4
Pedro Rogério <pinceladasdaweb@hotmail.com>
2020-10-17 16:34:06 -0300

    Fixed Cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,9 +9,13 @@ import routes from './routes'
 
 const app = express()
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 app.use(express.json())
 app.use(routes)
-app.use(cors())
 app.use(errorHanlder)
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
